ce90ad10852b866ba0f481037b369d0d93866e2f
Fredy <44968046+Liontek1985@users.noreply.github.com>
2023-02-03 15:37:46 +0100

    Update tekcommand.sh

--- tekcommand.sh
+++ tekcommand.sh
@@ -15,43 +15,40 @@ rp_module_id="tekcommand"
 rp_module_desc="Runcommand Launchscreens"
 rp_module_repo="git https://github.com/Liontek1985/tekcommand.git master"
 rp_module_section="main"
+rp_module_flags="noinstclean"
 
 function depends_tekcommand() {
-    getDepends mc
+    local depends=(cmake)
+     getDepends "${depends[@]}"
 }
 
 
 function sources_tekcommand() {
-    gitPullOrClone
+    if [[ -d "$md_inst" ]]; then
+        git -C "$md_inst" reset --hard  # ensure that no local changes exist
+    fi
+    gitPullOrClone "$md_inst"
 }
 
 function install_tekcommand() {
-    local tekdir="$scriptdir/scriptmodules/supplementary/tekcommand"
     local teksetup="$scriptdir/scriptmodules/supplementary"
-
-    mkdir -p "$tekdir"
-    chown -R $user:$user "$tekdir"
 	
-	cp -r -u "configs" "$tekdir/configs"
-    chown -R $user:$user "$tekdir"
+    cd "$md_inst"
 
-	cp -rvf "$tekdir/configs" "$rootdir"	
+	cp -rvf "configs" "$rootdir"	
     chown -R $user:$user "$rootdir/configs"
 	
 	cp -r -u "tekcommand.sh" "$teksetup/tekcommand.sh"
     chown -R $user:$user "$teksetup/tekcommand.sh"
 	chmod 755 "$teksetup/tekcommand.sh"
+	rm -r "tekcommand.sh"
 	
-}
-
-function configure_tekcommand() {
-    [[ "$md_mode" == "remove" ]] && return
 }
 
 
 function remove_tekcommand() {
 
-    rm -rf "$scriptdir/scriptmodules/supplementary/tekcommand"
+	rm -rf "$md_inst"
 	cd "$rootdir/configs"
 	find . -name "*launching.png" | sed -e "p;s/g.png/g.bkpng/" | xargs -n2 mv
 	find . -name "*launching.bkpng" -exec rm {} \;
